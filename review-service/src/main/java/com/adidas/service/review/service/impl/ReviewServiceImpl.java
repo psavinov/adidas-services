7e292dfbbea3f241ac91b344862a54c85953b765
pavel-savinov <pavel.savinov@liferay.com>
2018-12-03 19:42:21 +0100

    [REVIEW] Basic review service implementation

---
+++ review-service/src/main/java/com/adidas/service/review/service/impl/ReviewServiceImpl.java
@@ -0,0 +1,95 @@
+package com.adidas.service.review.service.impl;
+
+import com.adidas.service.review.dao.ReviewDao;
+import com.adidas.service.review.entity.Review;
+import com.adidas.service.review.exception.NoSuchReviewException;
+import com.adidas.service.review.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Pavel Savinov
+ */
+@Service
+public class ReviewServiceImpl implements ReviewService {
+
+	@Override
+	public Review addReview(
+		String productId, Integer reviewsNumber, Double averageScore) {
+
+		validate(productId, reviewsNumber, averageScore);
+
+		Review review = new Review();
+
+		review.setProductId(productId);
+		review.setReviewsNumber(reviewsNumber);
+		review.setAverageScore(averageScore);
+
+		return reviewDao.save(review);
+	}
+
+	@Override
+	public void deleteReview(String productId) {
+		reviewDao.deleteById(productId);
+	}
+
+	@Override
+	public List<Review> getReviews(Pageable pageable) {
+		Page<Review> page = reviewDao.findAll(pageable);
+
+		return page.get().collect(Collectors.toList());
+	}
+
+	@Override
+	public Review getReviewByProductId(String productId)
+		throws NoSuchReviewException {
+
+		return reviewDao.findById(productId).orElseThrow(
+			NoSuchReviewException::new);
+	}
+
+	@Override
+	public Review updateReview(
+			String productId, Integer reviewsNumber, Double averageScore)
+		throws NoSuchReviewException {
+
+		validate(productId, reviewsNumber, averageScore);
+
+		Review review = reviewDao.findById(productId).orElseThrow(
+			NoSuchReviewException::new);
+
+		review.setReviewsNumber(reviewsNumber);
+		review.setAverageScore(averageScore);
+
+		return reviewDao.save(review);
+	}
+
+	private void validate(
+		String productId, Integer reviewsNumber, Double averageScore) {
+
+		if (productId == null || productId.equals("") ||
+			reviewsNumber == null || averageScore == null) {
+
+			throw new IllegalArgumentException("Arguments must be not null");
+		}
+
+		if (reviewsNumber <= 0) {
+			throw new IllegalArgumentException(
+				"Reviews number must be greater than zero");
+		}
+
+		if (averageScore < 0) {
+			throw new IllegalArgumentException(
+				"Reviews number must be greater or equal to zero");
+		}
+	}
+
+	@Autowired
+	private ReviewDao reviewDao;
+
+}
