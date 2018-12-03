ccc71453b4f678f2730fcc4c325540f4523e3e8b
pavel-savinov <pavel.savinov@liferay.com>
2018-12-03 19:30:07 +0100

    [REVIEW] Review service with CRUD methods definition and corresponding exception

---
+++ review-service/src/main/java/com/adidas/service/review/exception/NoSuchReviewException.java
@@ -0,0 +1,7 @@
+package com.adidas.service.review.exception;
+
+/**
+ * @author Pavel Savinov
+ */
+public class NoSuchReviewException extends Exception {
+}
---
+++ review-service/src/main/java/com/adidas/service/review/service/ReviewService.java
@@ -0,0 +1,28 @@
+package com.adidas.service.review.service;
+
+import com.adidas.service.review.entity.Review;
+import com.adidas.service.review.exception.NoSuchReviewException;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * @author Pavel Savinov
+ */
+public interface ReviewService {
+
+	public Review addReview(
+		String productId, Integer reviewsNumber, Double averageScore);
+
+	public void deleteReview(String productId);
+
+	public List<Review> getReviews(Pageable pageable);
+
+	public Review getReviewByProductId(String productId)
+		throws NoSuchReviewException;
+
+	public Review updateReview(
+			String productId, Integer reviewsNumber, Double averageScore)
+		throws NoSuchReviewException;
+
+}
