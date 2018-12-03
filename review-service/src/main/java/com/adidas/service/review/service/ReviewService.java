2122c52b62ca78c77bd08c2790f01035819881a7
pavel-savinov <pavel.savinov@liferay.com>
2018-12-03 21:08:49 +0100

    [REVIEW] Add reviews cache

--- review-service/src/main/java/com/adidas/service/review/service/ReviewService.java
+++ review-service/src/main/java/com/adidas/service/review/service/ReviewService.java
@@ -2,6 +2,7 @@ package com.adidas.service.review.service;
 
 import com.adidas.service.review.entity.Review;
 import com.adidas.service.review.exception.NoSuchReviewException;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public interface ReviewService {
 
 	public List<Review> getReviews(Pageable pageable);
 
+	@Cacheable("reviews")
 	public Review getReviewByProductId(String productId)
 		throws NoSuchReviewException;
 
