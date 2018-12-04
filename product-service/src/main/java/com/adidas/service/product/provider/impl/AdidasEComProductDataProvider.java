208b2940f2bc8127296744927eff5c8582a18ef7
pavel-savinov <pavel.savinov@liferay.com>
2018-12-04 11:19:02 +0100

    [PRODUCT] Adidas ecom data provider

---
+++ product-service/src/main/java/com/adidas/service/product/provider/impl/AdidasEComProductDataProvider.java
@@ -0,0 +1,55 @@
+package com.adidas.service.product.provider.impl;
+
+import com.adidas.service.product.entity.Data;
+import com.adidas.service.product.entity.ProductData;
+import com.adidas.service.product.provider.DataProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.Serializable;
+
+/**
+ * @author Pavel Savinov
+ */
+@Service("adidasECom")
+public class AdidasEComProductDataProvider implements DataProvider {
+
+	@Override
+	public Data getData(Serializable resourceId) {
+		HttpComponentsClientHttpRequestFactory clientHttpRequestFactory =
+			new HttpComponentsClientHttpRequestFactory(
+				HttpClientBuilder.create().build());
+
+		RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+
+		HttpHeaders headers = new HttpHeaders();
+
+		headers.set("Accept", "*/*");
+		headers.set("Accept-Encoding", "gzip");
+		headers.set("Accept-Language", "en-US;en;");
+		headers.set("Connection", "keep-alive");
+		headers.set("DNT", "1");
+		headers.set("Host", "www.adidas.co.uk");
+		headers.set("Upgrade-Insecure-Requests", "1");
+		headers.set(
+			"User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5)");
+
+		HttpEntity entity = new HttpEntity(headers);
+
+		ResponseEntity<ProductData> response = restTemplate.exchange(
+			String.format("%s%s", endpoint, resourceId), HttpMethod.GET, entity,
+			ProductData.class, "");
+
+		return response.getBody();
+	}
+
+	@Value("${adidas.ecom.endpoint}")
+	private String endpoint;
+}
